e115390a0d273462633f59e5bbc808720cbfb57f
MarinoMtz <34484710+MarinoMtz@users.noreply.github.com>
2021-03-09 22:15:46 +0100

    Update SPI_reset.sh

--- SPI_reset/SPI_reset.sh
+++ SPI_reset/SPI_reset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "17"  > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio17/direction
-echo "1"   > /sys/class/gpio/gpio17/value
+echo "25"  > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio25/direction
+echo "1"   > /sys/class/gpio/gpio25/value
 sleep 5
-echo "0"   > /sys/class/gpio/gpio17/value
+echo "0"   > /sys/class/gpio/gpio25/value
 sleep 1
-echo "0"   > /sys/class/gpio/gpio17/value
+echo "0"   > /sys/class/gpio/gpio25/value
+echo "25"  > /sys/class/gpio/unexport
